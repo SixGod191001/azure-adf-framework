e7e383f6aba947faa9dab791b304cbc38b2a02c9
Administrator <gitlab_admin_e79808@example.com>
2024-07-09 09:30:13 +0800

    test

--- adf_metadata_db_sql_proj/metadata/Tables/Pipelines.sql
+++ adf_metadata_db_sql_proj/metadata/Tables/Pipelines.sql
@@ -5,6 +5,7 @@
     [PipelineName] NVARCHAR (200) NOT NULL,
     [LogicalPredecessorId] INT NULL,
     [Enabled]      BIT CONSTRAINT [DF_Pipelines_Enabled] DEFAULT ((1)) NOT NULL,
+    [BatchId] UNIQUEIDENTIFIER NULL,
     CONSTRAINT [PK_Pipelines] PRIMARY KEY CLUSTERED ([PipelineId] ASC),
     CONSTRAINT [FK_Pipelines_Stages] FOREIGN KEY ([StageId]) REFERENCES [metadata].[Stages] ([StageId]),
     CONSTRAINT [FK_Pipelines_Orchestrators] FOREIGN KEY([OrchestratorId]) REFERENCES [metadata].[Orchestrators] ([OrchestratorId]),
